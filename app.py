e605600568eae760535c25b5d8d4342ef2ac0ca6
Usama Navid <concaption@gmail.com>
2023-11-29 16:17:02 +0500

    Refactor code and add comments

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
-# Import necessary libraries
-import openai
-import streamlit as st
+"""
+path: app.py
+description:
+    This is the main file for the Streamlit app.
+    It contains the code for the chat interface and the logic to interact with the OpenAI API.
+"""
 import time
 import os
 import json
-import yfinance as yf   
+
+import openai
+import streamlit as st
+import yfinance as yf
 from dotenv import load_dotenv
+
 load_dotenv()
 
-def getStockPrice(ticker):
+def get_stock_price(ticker):
     """
-    Get the latest stock price from yahoo finance
-    :param ticker: ticker symbol for the stock
-    :return: latest stock price
+    This function returns the latest stock price for a given ticker symbol.
     """
-    # Fetch data for the given ticker symbol
     stock = yf.Ticker(ticker)
 
     # Get the latest closing price
@@ -24,8 +28,11 @@ def getStockPrice(ticker):
     return latest_price
 
 
-def handle_function(run):
-    tools_to_call = run.required_action.submit_tool_outputs.tool_calls
+def handle_function(a_run):
+    """
+    This function handles the required action of a run.
+    """
+    tools_to_call = a_run.required_action.submit_tool_outputs.tool_calls
     tools_output_array = []
     for each_tool in tools_to_call:
         tool_call_id = each_tool.id
@@ -35,21 +42,20 @@ def handle_function(run):
         print("Function to Call:" + function_name )
         print("Parameters to use:" + function_arg)
 
-        if (function_name == 'get_stock_price'):
+        if function_name == 'get_stock_price':
             arguments_str = each_tool.function.arguments
             arguments_dict = json.loads(arguments_str)
             symbol = arguments_dict['symbol']
             st.sidebar.write('get stock price for ', symbol)
 
-            output = getStockPrice(symbol)
+            output = get_stock_price(symbol)
             tools_output_array.append({"tool_call_id": tool_call_id, "output": output})
 
     client.beta.threads.runs.submit_tool_outputs(
         thread_id = st.session_state.thread_id,
-        run_id = run.id,
+        run_id = a_run.id,
         tool_outputs=tools_output_array
-    )    
-
+    )
 
 # Set your OpenAI Assistant ID here
 assistant_id = os.getenv('ASSISTANT_ID')
@@ -69,14 +75,15 @@ if "thread_id" not in st.session_state:
 
 # Set up the Streamlit page with a title and icon
 st.set_page_config(page_title="Smart Investment Advisor", page_icon=":moneybag:", layout="wide")
+URL = "https://platform.openai.com/account/api-keys"
+
 st.header(":moneybag: Smart Investment Advisor")
 
-#Get the OPENAI API Key
+# Get the OPENAI API Key
 openai_api_key_env = os.getenv('OPENAI_API_KEY')
 openai_api_key = st.sidebar.text_input(
     'OpenAI API Key', placeholder='sk-', value=openai_api_key_env)
-url = "https://platform.openai.com/account/api-keys"
-st.sidebar.markdown("Get an Open AI Access Key [here](%s). " % url)
+st.sidebar.markdown(f"Get an Open AI Access Key [here]({URL}). ")
 if openai_api_key:
     openai.api_key = openai_api_key
 
@@ -89,8 +96,11 @@ if st.sidebar.button("Start Chat"):
     st.write("thread id: ", thread.id)
 
 # Define the function to process messages with citations
-def process_message_with_citations(message):
-    message_content = message.content[0].text.value
+def process_message_with_citations(orignal_message):
+    """
+    This function processes the assistant's messages and replaces citations with the actual content.
+    """
+    message_content = orignal_message.content[0].text.value
     return message_content
 
 # Only show the chat interface if the chat has been started
@@ -122,7 +132,10 @@ if st.session_state.start_chat:
         run = client.beta.threads.runs.create(
             thread_id=st.session_state.thread_id,
             assistant_id=assistant_id,
-            instructions="Please answer the queries using the knowledge provided in the files. When adding other information mark it clearly as such.with a different color"
+            instructions="""
+                Please answer the queries using the knowledge provided in the files.
+                When adding other information mark it clearly as such.with a different color
+                """
         )
 
         # Poll for the run to complete and retrieve the assistant's messages
@@ -144,7 +157,7 @@ if st.session_state.start_chat:
 
         # Process and display assistant messages
         assistant_messages_for_run = [
-            message for message in messages 
+            message for message in messages
             if message.run_id == run.id and message.role == "assistant"
         ]
         for message in assistant_messages_for_run:
